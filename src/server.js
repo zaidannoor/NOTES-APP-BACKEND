b569f005c0847a99a5dbbd21eef8d4e1aaa04f3e
Zaidan Noor <zaidannoor06@gmail.com>
2023-03-21 01:48:46 +0700

    Initial Commit

---
+++ src/server.js
@@ -0,0 +1,30 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
+
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'], // an array of origins or 'ignore'
+        headers: ['Authorization'], // an array of strings - 'Access-Control-Allow-Headers'
+        exposedHeaders: ['Accept'], // an array of exposed headers - 'Access-Control-Expose-Headers',
+        additionalExposedHeaders: ['Accept'], // an array of additional exposed headers
+        maxAge: 60,
+        credentials: true,
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
